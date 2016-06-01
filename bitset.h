13109f673fd19f392f285073f6ed54ce7401bf96
nvcleemp <nico.vancleemput@gmail.com>
2016-06-01 13:42:48 +0200

    Added prefix to make macros more unique

--- bitset.h
+++ bitset.h
@@ -10,31 +10,31 @@
 
 typedef unsigned long long int bitset;
 
-#define ZERO 0ULL
-#define ONE 1ULL
-#define EMPTY_SET 0ULL
-#define CLEAR_SET(s) s = EMPTY_SET
-#define SINGLETON(el) (ONE << (el))
-#define IS_SINGLETON(s) ((s) && (!((s) & ((s)-1))))
-#define HAS_MORE_THAN_ONE_ELEMENT(s) ((s) & ((s)-1))
-#define IS_NOT_EMPTY(s) (s)
-#define IS_EMPTY(s) (!(s))
-#define CONTAINS(s, el) ((s) & SINGLETON(el))
-#define CONTAINS_ALL(s, elements) (((s) & (elements)) == (elements))
-#define ADD(s, el) ((s) |= SINGLETON(el))
-#define ADD_ALL(s, elements) ((s) |= (elements))
-#define UNION(s1, s2) ((s1) | (s2))
-#define INTERSECTION(s1, s2) ((s1) & (s2))
+#define BS_ZERO 0ULL
+#define BS_ONE 1ULL
+#define BS_EMPTY_SET 0ULL
+#define BS_CLEAR_SET(s) s = BS_EMPTY_SET
+#define BS_SINGLETON(el) (BS_ONE << (el))
+#define BS_IS_SINGLETON(s) ((s) && (!((s) & ((s)-1))))
+#define BS_HAS_MORE_THAN_ONE_ELEMENT(s) ((s) & ((s)-1))
+#define BS_IS_NOT_EMPTY(s) (s)
+#define BS_IS_EMPTY(s) (!(s))
+#define BS_CONTAINS(s, el) ((s) & BS_SINGLETON(el))
+#define BS_CONTAINS_ALL(s, elements) (((s) & (elements)) == (elements))
+#define BS_ADD(s, el) ((s) |= BS_SINGLETON(el))
+#define BS_ADD_ALL(s, elements) ((s) |= (elements))
+#define BS_UNION(s1, s2) ((s1) | (s2))
+#define BS_INTERSECTION(s1, s2) ((s1) & (s2))
 //these will only work if the element is actually in the set
-#define REMOVE(s, el) ((s) ^= SINGLETON(el))
-#define REMOVE_ALL(s, elements) ((s) ^= (elements))
-#define MINUS(s, el) ((s) ^ SINGLETON(el))
-#define MINUS_ALL(s, elements) ((s) ^ (elements))
+#define BS_REMOVE(s, el) ((s) ^= BS_SINGLETON(el))
+#define BS_REMOVE_ALL(s, elements) ((s) ^= (elements))
+#define BS_MINUS(s, el) ((s) ^ BS_SINGLETON(el))
+#define BS_MINUS_ALL(s, elements) ((s) ^ (elements))
 //the following macros perform an extra step, but will work even if the element is not in the set
-#define SAFE_REMOVE(s, el) ADD(s, el); REMOVE(s, el)
-#define SAFE_REMOVE_ALL(s, elements) ADD_ALL(s, elements); REMOVE_ALL(s, elements)
+#define BS_SAFE_REMOVE(s, el) BS_ADD(s, el); BS_REMOVE(s, el)
+#define BS_SAFE_REMOVE_ALL(s, elements) BS_ADD_ALL(s, elements); BS_REMOVE_ALL(s, elements)
 
 //the maximum capacity of a set
-#define SET_CAPACITY (sizeof(bitset)*8)
+#define BS_SET_CAPACITY (sizeof(bitset)*8)
 
 #endif	/* BITSET_H */
